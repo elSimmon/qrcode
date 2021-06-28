d2d862203037dde7bcc605ed12bd1f7ffee0ab37
eLSimmon <simmonshasbouncedbak@gmail.com>
2021-06-28 21:37:16 +0100

    Created migrations

---
+++ database/migrations/2021_06_28_190231_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function(Blueprint $table){
+          $table->id();
+          $table->integer('user_id');
+          $table->string('name');
+          $table->string('qrcode')->unique();
+          $table->string('imagepath');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
